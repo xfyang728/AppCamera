9d1767b76aa075a2a7a3216ef2c82c0a80d8a403
杨晓锋 <18120279@bjtu.edu.cn>
2023-05-21 12:45:48 +0800

    人脸检测 火焰检测

--- AppCamera/AppCamera.cpp
+++ AppCamera/AppCamera.cpp
@@ -11,6 +11,7 @@
 #include "cv_puttextzh.h"
 
 using namespace cv;
+using namespace std;
 
 void imgTest() {
 
@@ -28,10 +29,37 @@ void imgTest() {
 	cv::destroyWindow("测试opencv");
 }
 
+Mat FireDetectionHSV(Mat& image)	//利用HSV划分阈值进行火焰检测
+{
+	Mat imageHSV;
+	cvtColor(image, imageHSV, COLOR_BGR2HSV);
+	Mat BinaryImage = Mat::zeros(image.rows, image.cols, CV_8UC1);
+	for (int i = 0; i < imageHSV.rows; i++)
+	{
+		for (int j = 0; j < imageHSV.cols; j++)
+		{
+			int value_h = imageHSV.at<cv::Vec3b>(i, j)[0];
+			int value_s = imageHSV.at<cv::Vec3b>(i, j)[1];
+			int value_v = imageHSV.at<cv::Vec3b>(i, j)[2];
+			if ((value_h >= 0 && value_h <= 60) && (value_s <= 255 && value_s >= 160) && (value_v <= 255 && value_v >= 160))
+			{
+				BinaryImage.at<uchar>(i, j) = 255;
+				image.at<cv::Vec3b>(i, j)[0] = 0;
+				image.at<cv::Vec3b>(i, j)[1] = 255;
+				image.at<cv::Vec3b>(i, j)[2] = 0;
+			}
+		}
+	}
+
+	return BinaryImage;
+
+}
+
+
 int cameraTest() {
 
 	// 创建VideoCapture对象，参数为0表示打开默认摄像头
-	VideoCapture cap(0);
+	VideoCapture cap(1);
 
 	// 检查摄像头是否成功打开
 	if (!cap.isOpened())
@@ -65,6 +93,9 @@ int cameraTest() {
 			CV_RGB(255, 255, 255),
 			20
 		);
+
+		//frame = FireDetectionHSV(frame);
+
 		imshow("Camera", frame);
 
 		// 按下q键退出循环
@@ -79,6 +110,71 @@ int cameraTest() {
 	return 0;
 }
 
+int faceClassifierTest() {
+
+
+	// 加载人脸检测器
+	CascadeClassifier Classifier;
+	string Path = "D:/tools/opencv/build/etc/haarcascades/haarcascade_frontalface_default.xml";
+	Classifier.load(Path);
+	if (!Classifier.load(Path))   //加载xml文件  
+	{
+		cout << "xml文件加载失败" << endl;
+		return -1;
+	}
+
+	// 创建VideoCapture对象，参数为0表示打开默认摄像头
+	VideoCapture cap(1);
+	vector<Rect> faces;
+
+	// 检查摄像头是否成功打开
+	if (!cap.isOpened())
+	{
+		std::cout << "Failed to open camera!" << std::endl;
+		return -1;
+	}
+
+	// 创建窗口
+	namedWindow("Camera", WINDOW_NORMAL);
+
+	// 循环读取摄像头捕捉到的帧并显示
+	while (true)
+	{
+		// 读取一帧图像
+		Mat frame;
+		cap.read(frame);
+
+		// 检查是否成功读取到一帧图像
+		if (frame.empty())
+		{
+			std::cout << "Failed to read frame from camera!" << std::endl;
+			break;
+		}
+
+		// 显示图像
+		cv::putTextZH(frame, "Hello, 很赞", cv::Point(20, 20), CV_RGB(255, 255, 255), 20);
+
+		Classifier.detectMultiScale(frame, faces, 1.1, 3, 0, Size(30, 30));
+
+		for (size_t t = 0; t < faces.size(); t++)
+		{
+			rectangle(frame, faces[t], Scalar(0, 255, 0), 2, 8);//画个绿框
+		}
+		imshow("Camera", frame);
+		waitKey(10);//延时10ms
+
+
+					// 按下q键退出循环
+		if (waitKey(3) == 'q')
+			break;
+	}
+
+	// 释放摄像头资源和所有窗口
+	cap.release();
+	destroyAllWindows();
+
+}
+
 int main()
 {
 	//显示图像
@@ -86,5 +182,7 @@ int main()
 	//显示摄像头
 	int ret = cameraTest();
 
-	return ret;
+	faceClassifierTest();
+
+	return 0;
 }
